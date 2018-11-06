9459322d3bc48bc8d6ecc6857b010bb70221b93b
mynkow <mynkow@gmail.com>
2018-11-06 14:44:14 +0200

    Working build scripts

--- build-configuration.cmd
+++ build-configuration.cmd
@@ -1,6 +1,4 @@
 @echo off
 
-set nugetserver=https://marketvision.myget.org/F/qore-deployment/api/v2/package
-
 @powershell -File .nyx\build.ps1 '--appname=Elders.Cronus.Api' '--nugetPackageName=Cronus.Api'
 
--- build.cmd
+++ build.cmd
@@ -2,7 +2,6 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/nyx.cmd','.nyx\nyx.cmd')"
 call .nyx\nyx.cmd
 call .nyx\get-dependencies.cmd
-call build-configuration.cmd %1
 
 
 IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
