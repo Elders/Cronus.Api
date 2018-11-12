fa2b598a34ba48ceeaa93abf9ba2850a4e46e05b
mynkow <mynkow@gmail.com>
2018-11-12 17:02:32 +0200

    Fixes API hosting for windows

--- src/Elders.Cronus.Api/CronusApi.cs
+++ src/Elders.Cronus.Api/CronusApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Elders.Cronus.Api.Logging;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,9 +10,14 @@ namespace Elders.Cronus.Api
 {
     public static class CronusApi
     {
+        private static readonly ILog log = LogProvider.GetLogger(typeof(CronusApi));
+
         public static IWebHost GetHost(IConfigurationSource additionalConfiguration = null, string hostUrl = null)
         {
-            string url = hostUrl ?? "http://localhost:" + GetAvailablePort(9000) + "/";
+            int port = GetAvailablePort(9000);
+            string url = hostUrl ?? "http://+:" + port + "/";
+
+            log.Info(() => $"Starting Cronus API at {url}{Environment.NewLine}If you are not able to access it using DNS or public IP make sure that you have firewall rule and urlacl setup on the hosting machine.{Environment.NewLine} Example firewall: netsh advfirewall firewall add rule name=\"Cronus\" dir=in action=allow localport=9000-9010 protocol=tcp{Environment.NewLine} Example urlacl: netsh http add urlacl url={url} user=Everyone listen=yes");
 
             var webHostBuilder = WebHost
                 .CreateDefaultBuilder()
@@ -23,6 +29,8 @@ namespace Elders.Cronus.Api
                 webHostBuilder.ConfigureAppConfiguration(cfg => cfg.Add(additionalConfiguration));
 
             var webHost = webHostBuilder.Build();
+
+            log.Info($"Cronus API url: {url}");
 
             return webHost;
         }
