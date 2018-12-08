70e5cab8901000b1b5ec2210ecd40ee151ebf48e
mynkow <mynkow@gmail.com>
2018-12-08 18:49:16 +0200

    Adds an extension point to override Cronus services

--- src/Elders.Cronus.Api/CronusApi.cs
+++ src/Elders.Cronus.Api/CronusApi.cs
@@ -2,45 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using Elders.Cronus.Api.Logging;
+using Elders.Cronus.Discoveries;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Elders.Cronus.Api
 {
+    public class CronusApiBuilder
+    {
+        public Func<IServiceCollection, IConfiguration, CronusServicesProvider> CronusServicesProvider { get; set; }
+
+        public IConfigurationSource AdditionalConfigurationSource { get; set; }
+
+        public string HostUrl { get; set; }
+    }
+
     public static class CronusApi
     {
         private static readonly ILog log = LogProvider.GetLogger(typeof(CronusApi));
 
-        public static IWebHost GetHost(IConfigurationSource additionalConfiguration = null, string hostUrl = null)
+        public static IWebHost GetHost(Action<CronusApiBuilder> builder = null)
         {
+            var cronusApiBuilder = new CronusApiBuilder();
+            if (builder != null)
+                builder(cronusApiBuilder);
+
+
             int port = GetAvailablePort(9000);
-            string url = hostUrl ?? "http://+:" + port + "/";
+            string url = cronusApiBuilder.HostUrl ?? "http://+:" + port + "/";
 
             log.Info(() => $"Starting Cronus API at {url}{Environment.NewLine}If you are not able to access it using DNS or public IP make sure that you have firewall rule and urlacl setup on the hosting machine.{Environment.NewLine} Example firewall: netsh advfirewall firewall add rule name=\"Cronus\" dir=in action=allow localport=9000-9010 protocol=tcp{Environment.NewLine} Example urlacl: netsh http add urlacl url={url} user=Everyone listen=yes");
 
-            var webHostBuilder = WebHost
-                .CreateDefaultBuilder()
+            var webHostBuilder = WebHost.CreateDefaultBuilder();
+
+            if (cronusApiBuilder.AdditionalConfigurationSource is null == false)
+                webHostBuilder.ConfigureAppConfiguration(cfg => cfg.Add(cronusApiBuilder.AdditionalConfigurationSource));
+
+            webHostBuilder.ConfigureServices(services => services.AddSingleton<CronusApiBuilder>());
+
+            webHostBuilder
                 .UseKestrel()
                 .UseUrls(url)
                 .UseStartup<Startup>();
-
-            if (additionalConfiguration is null == false)
-                webHostBuilder.ConfigureAppConfiguration(cfg => cfg.Add(additionalConfiguration));
 
             var webHost = webHostBuilder.Build();
 
             log.Info($"Cronus API url: {url}");
 
             return webHost;
-        }
-
-        public static IWebHost Run(IConfigurationSource additionalConfiguration = null, string hostUrl = null)
-        {
-            var host = GetHost(additionalConfiguration, hostUrl);
-            host.RunAsync();
-
-            return host;
         }
 
         private static int GetAvailablePort(int startingPort)
