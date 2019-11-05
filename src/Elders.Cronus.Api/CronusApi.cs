f1c9d13984c54ec08abe90ff4fa07695ed3ccf37
mynkow <mynkow@gmail.com>
2019-11-05 20:37:50 +0200

    Removes obsolete method GetAvailablePort.

--- src/Elders.Cronus.Api/CronusApi.cs
+++ src/Elders.Cronus.Api/CronusApi.cs
@@ -71,39 +71,5 @@ namespace Elders.Cronus.Api
 
             return webHost;
         }
-
-        private static int GetAvailablePort(int startingPort)
-        {
-            System.Net.IPEndPoint[] endPoints;
-            List<int> portArray = new List<int>();
-
-            var properties = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties();
-
-            //getting active connections
-            System.Net.NetworkInformation.TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
-            portArray.AddRange(from n in connections
-                               where n.LocalEndPoint.Port >= startingPort
-                               select n.LocalEndPoint.Port);
-
-            //getting active tcp listners - WCF service listening in tcp
-            endPoints = properties.GetActiveTcpListeners();
-            portArray.AddRange(from n in endPoints
-                               where n.Port >= startingPort
-                               select n.Port);
-
-            //getting active udp listeners
-            endPoints = properties.GetActiveUdpListeners();
-            portArray.AddRange(from n in endPoints
-                               where n.Port >= startingPort
-                               select n.Port);
-
-            portArray.Sort();
-
-            for (int i = startingPort; i < UInt16.MaxValue; i++)
-                if (!portArray.Contains(i))
-                    return i;
-
-            return 0;
-        }
     }
 }
