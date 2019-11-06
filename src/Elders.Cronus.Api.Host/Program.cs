32d2c79584a15e46ee97ee6d5e8f90a5168be53f
mynkow <mynkow@gmail.com>
2019-11-06 21:20:56 +0200

    Adds a sample host where you could test the API

---
+++ src/Elders.Cronus.Api.Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using Elders.Cronus.Multitenancy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Elders.Cronus.Api.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var gg = builder.Sources.First();
+
+            var host = CronusApi.GetHost();
+
+            host.StartAsync();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
