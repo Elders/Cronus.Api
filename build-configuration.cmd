cff0027f7164b26b18e17b1dff86f8993c424994
mynkow <mynkow@gmail.com>
2021-05-07 08:58:42 +0300

    Switches to Azure pipelines and semantic release

--- build-configuration.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-@powershell -File .nyx\build.ps1 '--appname=Elders.Cronus.Api' '--nugetPackageName=Cronus.Api'
-@powershell -File .nyx\build.ps1 '--appname=Elders.Cronus.Dashboard' '--nugetPackageName=Cronus.Dashboard'
-
