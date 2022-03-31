4509008279b24170d431a73dfd86a062f2f57a8e
leksyCode <oleksii.nikolaiev@eldersoss.com>
2022-03-31 10:36:45 +0300

    fix: Migrate to 6.0.3

--- src/Elders.Cronus.Api/Startup.cs
+++ src/Elders.Cronus.Api/Startup.cs
@@ -106,7 +106,7 @@ namespace Elders.Cronus.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<RebuildProjectionHub>("/hub/projections");
+                endpoints.MapHub<RebuildProjectionHub>("/hub");
             });
         }
     }
