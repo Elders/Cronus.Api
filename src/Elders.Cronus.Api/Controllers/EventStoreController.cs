38ccc4f5e10f4f2a8e29b9fb3fbb389a24830337
KalinaShupeva <kalina.shupeva@eldersoss.com>
2023-06-20 15:38:38 +0300

    Introduces loading of raw events with paging

--- src/Elders.Cronus.Api/Controllers/EventStoreController.cs
+++ src/Elders.Cronus.Api/Controllers/EventStoreController.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using Elders.Cronus.EventStore.Index;
 using Microsoft.Extensions.Logging;
+using Elders.Cronus.EventStore;
 
 namespace Elders.Cronus.Api.Controllers
 {
@@ -43,10 +44,21 @@ namespace Elders.Cronus.Api.Controllers
             return new OkObjectResult(new ResponseResult<AggregateDto>(result));
         }
 
-        public class RequestModel
+        [HttpPost, Route("ExploreWithPaging")]
+        public async Task<IActionResult> ExploreWithPaging([FromBody] ExploreEventStoreWithPagingRequestModel model)
         {
-            [Required]
-            public string Id { get; set; }
+            ExploreWithPagingResponse result = ExploreWithPagingResponse.Empty();
+            try
+            {
+                result = await _eventExplorer.ExploreEventsWithPagingAsync(AggregateRootId.Parse(model.Id, Urn.Uber), model.PaginationToken, model.Take);
+
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorException(ex, () => $"Failed to explore aggregate with paging. Id: {model.Id}");
+            }
+
+            return new OkObjectResult(new ResponseResult<ExploreWithPagingResponse>(result));
         }
 
         [HttpPost, Route("Republish")]
@@ -91,6 +103,47 @@ namespace Elders.Cronus.Api.Controllers
             return new OkObjectResult(new ResponseResult());
         }
 
+        [HttpPost]
+        [Route("RepublishNew")]
+        public async Task<IActionResult> RepublishNew([FromBody] RepublishRequestNew model)
+        {
+            AggregateRootId id = AggregateRootId.Parse(model.Id, Urn.Uber);
+            IndexRecord record = new IndexRecord(model.EventContract, id.RawId, model.CommitRevision, model.EventPosition, model.Timestamp);
+            AggregateEventRaw rawEvent = await _eventExplorer.GetAggregateEventRaw(record).ConfigureAwait(false);
+
+            ReadOnlyMemory<byte> rawData = rawEvent.Data;
+
+            if (rawEvent is null)
+                return BadRequest("Event not found");
+
+            Type eventType = model.EventContract.GetTypeByContract();
+
+            if (model.IsPublicEvent)
+            {
+                Dictionary<string, string> headers = new Dictionary<string, string>()
+                {
+                    { MessageHeader.AggregateRootId,  id.ToBase64()}
+                };
+
+                publicPublisher.Publish(rawData, eventType, headers);
+            }
+            else
+            {
+                string recipientHandlers = ConcatRecipientHandlers(model.RecipientHandlers);
+                Dictionary<string, string> headers = new Dictionary<string, string>()
+                {
+                    { MessageHeader.AggregateRootId,  id.ToBase64()},
+                    { MessageHeader.AggregateRootRevision, model.CommitRevision.ToString()},
+                    { MessageHeader.AggregateRootEventPosition, model.EventPosition.ToString() },
+                    { MessageHeader.AggregateCommitTimestamp, rawEvent.Timestamp.ToString() },
+                    { MessageHeader.RecipientHandlers, string.Join(',', recipientHandlers) }
+            };
+                publisher.Publish(rawData, eventType, headers);
+            }
+
+            return new OkObjectResult(new ResponseResult());
+        }
+
         private string ConcatRecipientHandlers(string[] chosenRecipientHandlers)
         {
             string projectionIndexContract = typeof(ProjectionIndex).GetContractId();
@@ -114,6 +167,45 @@ namespace Elders.Cronus.Api.Controllers
             public int EventPosition { get; set; }
 
             public bool IsPublicEvent { get; set; }
+        }
+
+        public class RepublishRequestNew
+        {
+            [Required]
+            public string[] RecipientHandlers { get; set; }
+
+            [Required]
+            public string Id { get; set; }
+
+            [Required]
+            public int CommitRevision { get; set; }
+
+            [Required]
+            public int EventPosition { get; set; }
+
+            public bool IsPublicEvent { get; set; }
+
+            [Required]
+            public long Timestamp { get; set; }
+
+            [Required]
+            public string EventContract { get; set; }
+        }
+
+        public class ExploreEventStoreWithPagingRequestModel
+        {
+            [Required]
+            public string Id { get; set; }
+
+            public byte[] PaginationToken { get; set; }
+
+            public int Take { get; set; }
+        }
+
+        public class RequestModel
+        {
+            [Required]
+            public string Id { get; set; }
         }
     }
 }
---
+++ src/Elders.Cronus.Api/EventExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Elders.Cronus.Projections;
+
+namespace Elders.Cronus.Api
+{
+    public static class EventExtensions
+    {
+        public static IEnumerable<EventDto> ToEventsDto(this ProjectionCommit commit)
+        {
+            yield return commit.Event.ToEventDto(commit.TimeStamp);
+        }
+
+        public static ProjectionCommitDto ToProjectionDto(this ProjectionCommit commit)
+        {
+            return new ProjectionCommitDto()
+            {
+                Events = new List<EventDto> { commit.Event.ToEventDto(commit.TimeStamp) },
+                Timestamp = DateTime.FromFileTimeUtc(commit.EventOrigin.Timestamp)
+            };
+        }
+
+        public static EventDto ToEventDto(this IEvent @event, DateTimeOffset dateTimeOffset)
+        {
+            return @event.ToEventDto(dateTimeOffset, 1);
+        }
+
+        public static IEnumerable<EventDto> ToEventDto(this IEnumerable<IEvent> events, DateTimeOffset timestamp)
+        {
+            int eventPosition = 0;
+            foreach (IEvent @event in events)
+            {
+                yield return @event.ToEventDto(timestamp, eventPosition);
+                eventPosition++;
+            }
+        }
+
+        public static IEnumerable<EventDto> ToEventDto(this List<IEvent> events, DateTimeOffset timestamp)
+        {
+            int eventPosition = 0;
+            foreach (IEvent @event in events)
+            {
+                yield return @event.ToEventDto(timestamp, eventPosition);
+                eventPosition++;
+            }
+        }
+
+        public static EventDto ToEventDto(this IEvent @event, DateTimeOffset timestamp, int position)
+        {
+            var entityEvent = @event as EntityEvent;
+            if (entityEvent is null)
+            {
+                return new EventDto()
+                {
+                    Id = @event.GetType().GetContractId(),
+                    EventName = @event.GetType().Name,
+                    EventData = @event,
+                    EventPosition = position,
+                    IsPublicEvent = typeof(IPublicEvent).IsAssignableFrom(@event.GetType()),
+                    Timestamp = timestamp
+                };
+            }
+            else
+            {
+                return new EventDto()
+                {
+                    Id = entityEvent.Event.GetType().GetContractId(),
+                    EventName = entityEvent.Event.GetType().Name,
+                    EventData = entityEvent.Event,
+                    IsEntityEvent = true,
+                    EventPosition = 1,
+                    IsPublicEvent = typeof(IPublicEvent).IsAssignableFrom(@event.GetType()),
+                    Timestamp = timestamp
+                };
+            }
+        }
+
+        public static RawEventDto ToRawEventDto(this IMessage @event, DateTimeOffset timestamp, int position, int revision)
+        {
+            var entityEvent = @event as EntityEvent;
+            if (entityEvent is null)
+            {
+                return new RawEventDto()
+                {
+                    Id = @event.GetType().GetContractId(),
+                    EventName = @event.GetType().Name,
+                    EventData = @event,
+                    IsEntityEvent = false,
+                    EventPosition = position,
+                    EventRevision = revision,
+                    IsPublicEvent = typeof(IPublicEvent).IsAssignableFrom(@event.GetType()),
+                    Timestamp = timestamp
+                };
+            }
+            else
+            {
+                return new RawEventDto()
+                {
+                    Id = entityEvent.Event.GetType().GetContractId(),
+                    EventName = entityEvent.Event.GetType().Name,
+                    EventData = entityEvent.Event,
+                    IsEntityEvent = true,
+                    EventPosition = position,
+                    EventRevision = revision,
+                    IsPublicEvent = typeof(IPublicEvent).IsAssignableFrom(@event.GetType()),
+                    Timestamp = timestamp
+                };
+            }
+        }
+
+        public static IEnumerable<EventDto> ToPublicEventDto(this IEnumerable<IPublicEvent> events, int lastEventPosition)
+        {
+            int eventPosition = lastEventPosition + 5;
+            foreach (IPublicEvent @event in events)
+            {
+                yield return new EventDto()
+                {
+                    Id = @event.GetType().GetContractId(),
+                    EventName = @event.GetType().Name,
+                    EventData = @event,
+                    EventPosition = eventPosition,
+                    IsPublicEvent = true
+                };
+
+                eventPosition++;
+            }
+        }
+    }
+}
