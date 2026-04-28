65af94c7a8abb3bd27456dc364d138b1bbb74797
Kalin Venkov <kalin.venkov@eldersoss.com>
2026-04-28 17:03:39 +0300

    fix: Drops obsolete ISystemClock from BasicAuthenticationHandler

--- src/Elders.Cronus.Api/Security/BasicAuthenticationHandler.cs
+++ src/Elders.Cronus.Api/Security/BasicAuthenticationHandler.cs
@@ -9,8 +9,8 @@ namespace Elders.Cronus.Api.Security
 {
     public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
-            : base(options, logger, encoder, clock)
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder)
+            : base(options, logger, encoder)
         {
         }
 
